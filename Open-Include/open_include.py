501bd3befb5b4a7624204104f216c93fde1b3b44
Aaron Lademann <aaron.lademann@gmail.com>
2014-06-06 14:20:34 -0700

    Tweaks to Open-Include package

--- Open-Include/open_include.py
+++ Open-Include/open_include.py
@@ -17,6 +17,61 @@ IMAGE = re.compile('\.(apng|png|jpg|gif|jpeg|bmp)$', re.I)
 # global settings container
 s = None
 
+debug = False
+ST2 = int(sublime.version()) < 3000
+cache = {}
+
+def reset_cache():
+    global cache
+    if debug:
+        print('reset cache')
+    cache = {}
+    cache['os_listdir'] = {}
+    cache['os_exists'] = {}
+    cache['os_is_file'] = {}
+    cache['os_is_dir'] = {}
+    cache['done'] = {}
+    cache['look_into_folders'] = False
+    cache['running'] = False
+
+reset_cache()
+
+def normalize(path):
+    return path.lower().replace('\\', '/').replace('\\', '/')
+
+def os_listdir(path):
+    global cache
+    id = normalize(path)
+    if id not in cache['os_listdir']:
+        try:
+            cache['os_listdir'][id] = [os.path.join(path, x) for x in os.listdir(path) if os_is_dir(os.path.join(path, x))]
+        except:
+            cache['os_listdir'][id] = []
+    return cache['os_listdir'][id]
+
+def os_exists(path):
+    global cache
+    id = normalize(path)
+    if id not in cache['os_exists']:
+        try:
+            cache['os_exists'][id] = os.path.lexists(path)
+        except:
+            cache['os_exists'][id] = False
+    return cache['os_exists'][id]
+
+def os_is_file(path):
+    global cache
+    id = normalize(path)
+    if id not in cache['os_is_file']:
+        cache['os_is_file'][id] = os_exists(path) and os.path.isfile(path)
+    return cache['os_is_file'][id]
+
+def os_is_dir(path):
+    global cache
+    id = normalize(path)
+    if id not in cache['os_is_dir']:
+        cache['os_is_dir'][id] = os_exists(path) and os.path.isdir(path)
+    return cache['os_is_dir'][id]
 
 def plugin_loaded():
     global s
@@ -24,76 +79,121 @@ def plugin_loaded():
 
 
 class OpenInclude(sublime_plugin.TextCommand):
+    def run(self, edit = None):
+        global cache
+        if not cache['running']:
+            if ST2:
+                OpenIncludeThread().run()
+            else:
+                OpenIncludeThread().start()
 
-    # run and look for different sources of paths
-    def run(self, edit):
+class OpenIncludeThread(threading.Thread):
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        global cache
+        cache['running'] = True
+        if debug:
+            print('running')
         window = sublime.active_window()
-        view = self.view
+        view = window.active_view()
         something_opened = False
 
         for region in view.sel():
             opened = False
 
+            if debug:
+                print('------------------------------------')
+
             # find in files panel
             if not opened and 'Find Results.hidden-tmLanguage' in view.settings().get('syntax'):
-            	opened = OpenIncludeFindInFileGoto().run(view);
-
-            # between quotes
-            if not opened and view.score_selector(region.begin(), "parameter.url, string.quoted"):
-                file_to_open = view.substr(view.extract_scope(region.begin()))
-                opened = self.resolve_path(window, view, file_to_open)
-
-                if opened:
-                    break
-
-                if not opened and s.get('create_if_not_exists') and view.file_name():
-                    file_name = view.substr(view.extract_scope(region.begin())).replace("'", '').replace('"', '')
-                    path = self.resolve_relative(os.path.dirname(view.file_name()), file_name)
-                    branch, leaf = os.path.split(path)
-                    try:
-                        os.makedirs(branch)
-                    except:
-                        pass
-                    window.open_file(path)
-                    opened = True
-
-            # word
-            if not opened:
-                file_to_open = view.substr(view.word(region)).strip()
-                opened = self.resolve_path(window, view, file_to_open)
+                opened = OpenIncludeFindInFileGoto().run(view);
 
             # selected text
             if not opened:
+                if debug:
+                    print('selected')
+                    print(view.substr(region))
                 opened = self.resolve_path(window, view, view.substr(region))
+
+            # quoted
+            if not opened and view.score_selector(region.begin(), "parameter.url, string.quoted"):
+                file_to_open = view.substr(view.extract_scope(region.begin())).replace('"', '').replace("'", '')
+                if debug:
+                    print('quotes')
+                opened = self.resolve_path(window, view, file_to_open)
+
+                if not opened and s.get('create_if_not_exists') and view.file_name():
+                    if re.match('https?://', file_to_open):
+                    	pass
+                    else:
+	                    file_to_open = self.resolve_relative(os.path.dirname(view.file_name()), file_to_open)
+	                    branch, leaf = os.path.split(file_to_open)
+	                    try:
+	                        os.makedirs(branch)
+	                    except:
+	                        pass
+	                    self.open(window, file_to_open)
+	                    opened = True
+
+            # selection expanded to full lines
+            if not opened:
+                expanded_lines = view.substr(sublime.Region(view.line(region.begin()).begin(), view.line(region.end()).end()))
+                if debug:
+                    print('expanded lines')
+                opened = self.resolve_path(window, view, expanded_lines)
 
             # current line quotes and parenthesis
             if not opened:
                 line = view.substr(view.line(region.begin()))
-                for line in re.split(r"[()'\"]", line):
+                if debug:
+                    print('line')
+
+                for line in re.split("[(){}'\"]", line):
                     line = line.strip()
                     if line:
                         opened = self.resolve_path(window, view, line)
                         if opened:
                             break
 
-            # selection expanded to full lines
+            # split by spaces and tabs
             if not opened:
-                expanded_lines = view.substr(sublime.Region(view.line(region.begin()).begin(), view.line(region.end()).end()))
-                opened = self.resolve_path(window, view, expanded_lines)
+                words = re.sub("\s+", "\n", expanded_lines)  # expanded_lines.replace('\t', '\n').replace(' ', '\n'))
+                opened = self.resolve_path(window, view, words)
 
-                # split by spaces and tabs
-                if not opened:
-                    words = re.sub(r"\s+", "\n", expanded_lines)  # expanded_lines.replace('\t', '\n').replace(' ', '\n'))
-                    opened = self.resolve_path(window, view, words)
+            # word
+            if not opened:
+                file_to_open = view.substr(view.word(region)).strip()
+                if debug:
+                    print('word')
+                opened = self.resolve_path(window, view, file_to_open)
 
             if opened:
                 something_opened = True
 
         # Nothing in a selected region could be opened
         if not something_opened:
-            opened = self.resolve_path(window, view, view.substr(sublime.Region(0, view.size())).replace('\t', '\n'))
+            # run again, and look into every subfolder and it its parent folders
+            if not cache['look_into_folders']:
+                cache['look_into_folders'] = True
+                if debug:
+                    print('running again')
+                opened = self.run()
+            else:
+                if debug:
+                    print('looking into the whole view')
+                opened = self.resolve_path(window, view, view.substr(sublime.Region(0, 10485760 if view.size() > 10485760 else view.size())).replace('\t', '\n'), True)
+            reset_cache()
             if not opened:
-            	sublime.status_message("Unable to find a file in the current selection")
+                sublime.status_message("Unable to find a file in the current selection")
+                return False
+            else:
+                return True
+        else:
+            reset_cache()
+            return True
 
     def expand_paths_with_extensions(self, window, view, paths):
 
@@ -114,8 +214,51 @@ class OpenInclude(sublime_plugin.TextCommand):
                 path_add.append(os.path.join(*subs))
         return paths + path_add
 
+    def expand_paths_with_sub_and_parent_folders(self, window, view, paths):
+
+        paths2 = []
+        for path in paths:
+            paths2.append(path)
+        if not view.file_name():
+            # folders of opened views
+            for _view in window.views():
+                if _view.file_name():
+                    branch, leaf = os.path.split(_view.file_name())
+                    for path in paths:
+                        paths2.append(os.path.join(branch, path))
+        else:
+            # subfolders
+            branch, leaf = os.path.split(view.file_name())
+            for dir in os_listdir(branch):
+                for path in paths:
+                    paths2.append(os.path.join(dir, path))
+
+            # parent folders
+            branch, leaf = os.path.split(branch)
+            for dir in os_listdir(branch):
+                for path in paths:
+                    paths2.append(os.path.join(dir, path))
+
+            # folders of opened views
+            for _view in window.views():
+                if _view.file_name():
+                    branch, leaf = os.path.split(_view.file_name())
+                    for path in paths:
+                        paths2.append(os.path.join(branch, path))
+
+        # subfolders of the project folders
+        for branch in window.folders():
+            for dir in os_listdir(branch):
+                for path in paths:
+                    paths2.append(os.path.join(dir, path))
+
+        return list(set(paths2))
+
     # resolve the path of these sources and send to try_open
-    def resolve_path(self, window, view, paths):
+    def resolve_path(self, window, view, paths, skip_folders = False):
+        global cache
+        if debug:
+            print(paths)
         try:
             paths_decoded = urllib.unquote(paths.encode('utf8'))
             paths_decoded = unicode(paths_decoded.decode('utf8'))
@@ -123,22 +266,30 @@ class OpenInclude(sublime_plugin.TextCommand):
         except:
             pass
 
-        paths = paths.split('\n')
+        paths = paths.strip().split('\n')
+
+        if re.match(r'https?://', paths[0]):
+            return self.try_open(window, paths[0])
 
         if s.get('use_strict'):
             return self.try_open(window, self.resolve_relative(os.path.dirname(view.file_name()), paths[0]))
 
         paths = self.expand_paths_with_extensions(window, view, paths)
+        if cache['look_into_folders'] and not skip_folders:
+            paths = self.expand_paths_with_sub_and_parent_folders(window, view, paths)
 
         something_opened = False
 
+        paths = list(set(paths))
+
         for path in paths:
             path = path.strip()
-            if path == '':
+            if path == '' or path in cache['done']:
                 continue
+            cache['done'][path] = True
 
             # remove quotes
-            path = path.strip('"\'<>')  # re.sub(r'^("|\'|<)|("|\'|>)$', '', path)
+            path = path.strip('"\'<>\(\)\{\}')  # re.sub(r'^("|\'|<)|("|\'|>)$', '', path)
 
             # remove :row:col
             path = re.sub('(\:[0-9]*)+$', '', path).strip()
@@ -155,10 +306,6 @@ class OpenInclude(sublime_plugin.TextCommand):
                         maybe_path = os.path.dirname(maybe_path)
                         opened = self.create_path_relative_to_folder(window, view, maybe_path, new_path_prefix + path)
 
-                        
-                    # relative to view in static/_includes directory
-                    if not opened:
-                        opened = self.create_path_relative_to_folder(window, view, maybe_path, new_path_prefix + "./static/_includes/" + path)
                     if opened:
                         break
 
@@ -175,8 +322,6 @@ class OpenInclude(sublime_plugin.TextCommand):
             # absolute
             if not opened:
                 opened = self.try_open(window, path)
-                if opened:
-                    opened = True
 
             if opened:
                 something_opened = True
@@ -200,12 +345,13 @@ class OpenInclude(sublime_plugin.TextCommand):
                 webbrowser.open_new_tab(maybe_path)
             else:
                 sublime.status_message("Opening URL " + maybe_path)
+
                 # Create thread to download url in background
                 threading.Thread(target=self.read_url, args=(maybe_path,)).start()
 
-        elif os.path.isfile(maybe_path):
+        elif os_is_file(maybe_path):
             if IMAGE.search(maybe_path):
-                window.open_file(maybe_path)
+                self.open(window, maybe_path)
             elif BINARY.search(maybe_path):
                 try:
                     import desktop
@@ -214,7 +360,7 @@ class OpenInclude(sublime_plugin.TextCommand):
                 desktop.open(maybe_path)
             else:
                 # Open within ST
-                window.open_file(maybe_path)
+                self.open(window, maybe_path)
             sublime.status_message("Opening file " + maybe_path)
         else:
             return False
@@ -271,6 +417,12 @@ class OpenInclude(sublime_plugin.TextCommand):
             elif content_type == 'text/xml' or content_type == 'application/xml':
                 view.settings().set('syntax', 'Packages/XML/XML.tmLanguage')
 
+    def open(self, window, path):
+        if s.get('in_secondary_colum', False):
+            window.run_command('set_layout', {"cols": [0.0, 0.5, 1.0], "rows": [0.0, 1.0], "cells": [[0, 0, 1, 1], [1, 0, 2, 1]]})
+            window.focus_group(1)
+        window.open_file(path)
+
 class OpenIncludeFindInFileGoto():
     def run(self, view):
         line_no = self.get_line_no(view)
@@ -299,7 +451,7 @@ class OpenIncludeFindInFileGoto():
                 line_text = view.substr(line)
                 match = re.match(r"^(.+)\:$", line_text)
                 if match:
-                    if os.path.exists(match.group(1)):
+                    if os_exists(match.group(1)):
                         return match.group(1)
                 line = view.line(line.begin() - 1)
         return None
